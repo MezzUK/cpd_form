a60277af8e7199be572483e7812a3909db350c06
MezzUK <bbadcock_uk@yahoo.com>
2020-04-05 11:06:05 +0100

    User specific limiting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :update, :show]
+
     def index
         @users = User.all
     end
@@ -18,11 +20,9 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:success] = "Your account was updated successfully"
             redirect_to articles_path
@@ -32,11 +32,13 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
     end
 
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+    def set_user
+        @user = User.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   #post 'signup', to: 'articles#index'
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
